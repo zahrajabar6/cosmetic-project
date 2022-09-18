f0e4f2e806c1f037ca3b9cc85f6c71feac168120
Zahar Jabar <zzooz162001@gmail.com>
2022-09-18 18:35:44 +0300

    edit

--- lib/view/login_Signup_pages/login_page.dart
+++ lib/view/login_Signup_pages/login_page.dart
@@ -25,64 +25,66 @@ class _MyLoginState extends State<MyLogin> {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Form(
             key: formKey,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 30),
-                    child: Image.asset(
-                      'images/logo.png',
-                      width: 126,
-                      height: 126,
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30),
+                      child: Image.asset(
+                        'images/logo.png',
+                        width: 126,
+                        height: 126,
+                      ),
                     ),
-                  ),
-                  const MyHeadingText(text: 'Login to your account'),
-                  MyTextFormField(
-                      obsecure: false,
-                      label: 'Email',
-                      validator: (x) {
-                        String pattern =
-                            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-                            r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-                            r"{0,253}[a-zA-Z0-9])?)*$";
-                        RegExp regex = RegExp(pattern);
-                        if (x!.isEmpty || !regex.hasMatch(x)) {
-                          return 'Enter a valid email address';
-                        } else {
-                          return null;
-                        }
-                      }),
-                  MyTextFormField(
-                      obsecure: true,
-                      label: 'Password',
-                      validator: (x) {
-                        if (x!.length < 8 || x.isEmpty) {
-                          return 'Wrong password';
-                        } else {
-                          return null;
-                        }
-                      }),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: MyButton(
-                      text: 'Sign In',
-                      onPress: () {
-                        if (formKey.currentState!.validate()) {
-                          Get.to(const MyPages());
-                        }
-                      },
+                    const MyHeadingText(text: 'Login to your account'),
+                    MyTextFormField(
+                        obsecure: false,
+                        label: 'Email',
+                        validator: (x) {
+                          String pattern =
+                              r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                              r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                              r"{0,253}[a-zA-Z0-9])?)*$";
+                          RegExp regex = RegExp(pattern);
+                          if (x!.isEmpty || !regex.hasMatch(x)) {
+                            return 'Enter a valid email address';
+                          } else {
+                            return null;
+                          }
+                        }),
+                    MyTextFormField(
+                        obsecure: true,
+                        label: 'Password',
+                        validator: (x) {
+                          if (x!.length < 8 || x.isEmpty) {
+                            return 'Wrong password';
+                          } else {
+                            return null;
+                          }
+                        }),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: MyButton(
+                        text: 'Sign In',
+                        onPress: () {
+                          if (formKey.currentState!.validate()) {
+                            Get.to(const MyPages());
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  const MyTextButton(
-                    text: "Don't have account? Sign up",
-                    page: RegisterPage(),
-                  ),
-                  const MyTextButton(
-                    text: "or Continue as a guest",
-                    page: MyPages(),
-                  )
-                ]),
+                    const MyTextButton(
+                      text: "Don't have account? Sign up",
+                      page: RegisterPage(),
+                    ),
+                    const MyTextButton(
+                      text: "or Continue as a guest",
+                      page: MyPages(),
+                    )
+                  ]),
+            ),
           ),
         ),
       ),
